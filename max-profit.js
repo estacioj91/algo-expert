56ca478733e81298212d8d10aabc85e3abe04492
jonathan estacio <estacio@jonathans-MBP.hsd1.ca.comcast.net>
2020-12-03 21:28:00 -0800

    max-profit comments

--- max-profit.js
+++ max-profit.js
@@ -3,8 +3,10 @@ var maxProfit = function (prices) {
 	var maxProfit = 0;
 	var minVal = Infinity;
 	prices.forEach((value) => {
+		//7|infinity, 1|7, 5|1,
 		if (value < minVal) {
 			minVal = value;
+			//5 - 1,
 		} else if (value - minVal > maxProfit) {
 			maxProfit = value - minVal;
 		}
